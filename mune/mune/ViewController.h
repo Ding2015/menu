f84049daf666b2d81bf6f91f9aa94a7c10a8f9cb
就不给你上 <747985559@qq.com>
2015-09-21 09:32:04 +0800

    代码

---
+++ mune/mune/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mune
+//
+//  Created by XZH on 15/9/21.
+//  Copyright (c) 2015年 就不给你上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mune/mune/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mune
+//
+//  Created by XZH on 15/9/21.
+//  Copyright (c) 2015年 就不给你上. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
