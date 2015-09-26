d5ae9774ef2e1b16014a50f1be66ff1382050197
Ding2015 <779734519@qq.com>
2015-09-26 08:28:47 +0800

    1

--- mune/mune/TableViewCell.m
+++ mune/mune/TableViewCell.m
@@ -10,6 +10,8 @@
 #import "TableViewCell.h"
 
 @implementation TableViewCell
+- (IBAction)XD:(UIButton *)sender forEvent:(UIEvent *)event {
+}
 
 - (void)awakeFromNib {
     // Initialization code
---
+++ mune/mune/listCell.h
@@ -0,0 +1,13 @@
+//
+//  listCell.h
+//  mune
+//
+//  Created by XZH on 15/9/25.
+//  Copyright (c) 2015年 就不给你上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface listCell : UITableViewCell
+
+@end
